514d22a27fd15bae862cdab2126001d08edf9b18
tsu2000 <106811131+tsu2000@users.noreply.github.com>
2023-07-03 00:41:02 +0800

    Update sql_playground.py

--- sql_playground.py
+++ sql_playground.py
@@ -27,7 +27,7 @@ def main():
     st.markdown('A simple web app for learning the basics of SQL using a sample database and schemas from a MySQL server. Only one SQL query can be executed at the time. To execute a SQL query, type your query into the text area and click `Execute` to see the results. There are currently **2** questions available to practice basic SQL queries. **Note:** Do not use the `USE` keyword when referring to different schemas. Instead, select tables using the `schema + table` notation. (i.e. `sql_store.customers`)')
 
     # Connect to MySQL Database
-    conn = st.experimental_connection('mysql', type = 'sql')
+    conn = st.experimental_connection('mydb', type = 'sql')
     
     opt = st.selectbox('Select a feature:', ['All MySQL Query Practice Questions', 'About Database'])
 
